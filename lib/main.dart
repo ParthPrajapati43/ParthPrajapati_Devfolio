399187416e865ded5358faffe86ca0065bbf88b0
Parth Prajapati <parthy4399@gmail.com>
2021-09-30 11:03:09 +0530

    theme provider modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:parth_prajapati_devfolio/provider/theme_styles.dart';
+import 'package:parth_prajapati_devfolio/provider/theme_provider.dart';
+import 'package:parth_prajapati_devfolio/sections/main_section.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -12,12 +17,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+ThemeProvider _themeProvider = ThemeProvider();
+
+  void getCurrentAppTheme() async {
+    _themeProvider.lightTheme = await _themeProvider.darkThemePref.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Parth',
-      home: Scaffold(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Parth',
+        theme: ThemeStyles.themeData(_themeProvider.lightTheme, context),
+        initialRoute: "/",
+        routes: {
+          "/": (context) => MainSection(),
+        },
+      ),
     );
   }
 }
