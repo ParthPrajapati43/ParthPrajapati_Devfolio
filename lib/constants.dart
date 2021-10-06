6f97e729832f2c7371e8763b2a8fde4393f4300d
Parth Prajapati <parthy4399@gmail.com>
2021-10-06 13:33:51 +0530

    Navigation Bar Added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // Colors
-const Color kPrimaryColor = Color(0xffC0392B);
+final Color kPrimaryColor = Colors.green;
+
+// URL Launcher
+void launchURL(String _url) async =>
+    await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
 
 
--- lib/sections/main_section.dart
+++ lib/sections/main_section.dart
@@ -1,17 +1,301 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:parth_prajapati_devfolio/animations/entrance_fader.dart';
+import 'package:parth_prajapati_devfolio/constants.dart';
+import 'package:parth_prajapati_devfolio/sections/navBar/nav_bar_logo.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/rendering.dart';
+import 'package:parth_prajapati_devfolio/provider/theme_provider.dart';
+import 'package:universal_html/html.dart' as html;
 
-class MainPage extends StatefulWidget {
-  const MainPage({ Key key }) : super(key: key);
+class MainSection extends StatefulWidget {
+  const MainSection({ Key key }) : super(key: key);
 
   @override
-  _MainPageState createState() => _MainPageState();
+  _MainSectionState createState() => _MainSectionState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainSectionState extends State<MainSection> {
+  ThemeProvider _themeProviders = ThemeProvider();
+  bool isPressed = false;
+  bool _isScrollingDown = false;
+  ScrollController _scrollController = ScrollController();
+
+  final List<String> _sectionsName = [
+    "HOME",
+    "ABOUT",
+    "SKILLS",
+    "EXPERIENCE",
+    "PROJECTS", // library_books
+    "ACHIEVEMENTS", //celebration auto_awesome emoji_events
+    "CONTACT" //contact_page
+  ];
+
+  final List<IconData> _sectionsIcons = [
+    Icons.home,
+    Icons.person,
+    Icons.verified,
+    Icons.work,
+    Icons.library_books,
+    Icons.emoji_events,
+    Icons.contact_page,
+    Icons.phone,
+  ];
+
+  void _scroll(int i) {
+    _scrollController.animateTo(
+      i == 0
+          ? 0.0
+          : i == 1
+              ? MediaQuery.of(context).size.height * 1.05
+              : i == 2
+                  ? MediaQuery.of(context).size.height * 1.98
+                  : i == 3
+                      ? MediaQuery.of(context).size.height * 2.9
+                      : MediaQuery.of(context).size.height * 4,
+      duration: const Duration(seconds: 1),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  Widget sectionWidget(int i) {
+    return Container();
+    /* if (i == 0) {
+      return Home();
+    } else if (i == 1) {
+      return About();
+    } else if (i == 2) {
+      return Skills();
+    } else if (i == 3) {
+      return Experience();
+    } else if (i == 4) {
+      return Projects();
+    } else if (i == 5){
+      return Achievements()
+    } else if (i == 6){
+      return Contacts();
+    } else if (i == 7) {
+      return Footer();
+    } else {
+      return Container();
+    } */
+  }
+
+  @override
+  void initState() {
+    _scrollController = _themeProviders.scroll;
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        if (!_isScrollingDown) {
+          _isScrollingDown = true;
+          setState(() {});
+        }
+      }
+
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
+        if (_isScrollingDown) {
+          _isScrollingDown = false;
+          setState(() {});
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _scrollController.removeListener(() {});
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final _themeProv = Provider.of<ThemeProvider>(context);
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: _themeProv.lightTheme ? Colors.white : Colors.black,
+      appBar: MediaQuery.of(context).size.width < 900
+          ? AppBar(
+              iconTheme: IconThemeData(
+                  color: _themeProv.lightTheme ? Colors.black : Colors.white),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              actions: [
+                NavBarLogo(),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.05,
+                )
+              ],
+            )
+          : _appBarTabDesktop(_themeProv),
+      drawer: MediaQuery.of(context).size.width < 900
+          ? _appBarMobile(_themeProv)
+          : null,
+    );
+  }
+
+  Widget _appBarActions(
+      String childText, int index, IconData icon, ThemeProvider themeProvider) {
+    return MediaQuery.of(context).size.width > 760
+        ? EntranceFader(
+            offset: Offset(0, 0),
+            delay: Duration(milliseconds: 100),
+            duration: Duration(milliseconds: 250),
+            child: Container(
+              padding: const EdgeInsets.all(0.0),
+              height: 60.0,
+              child: MaterialButton(
+                hoverColor: kPrimaryColor,
+                onPressed: () => _scroll(index),
+                child: Text(
+                  childText,
+                  style: TextStyle(
+                    color:
+                        themeProvider.lightTheme ? Colors.black : Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          )
+        : Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: MaterialButton(
+              hoverColor: kPrimaryColor.withAlpha(70),
+              onPressed: () {
+                _scroll(index);
+                Navigator.pop(context);
+              },
+              child: ListTile(
+                leading: Icon(
+                  icon,
+                  color: kPrimaryColor,
+                ),
+                title: Text(childText,
+                    style: TextStyle(
+                      color: themeProvider.lightTheme
+                          ? Colors.black
+                          : Colors.white,
+                    )),
+              ),
+            ),
+          );
+  }
+
+  Widget _appBarTabDesktop(ThemeProvider _themeProv) {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: _themeProv.lightTheme ? Colors.white : Colors.black,
+      title: MediaQuery.of(context).size.width < 760
+          ? EntranceFader(
+              duration: Duration(milliseconds: 250),
+              offset: Offset(0, -10),
+              delay: Duration(seconds: 3),
+              child: NavBarLogo(
+                height: 20.0,
+              ))
+          : EntranceFader(
+              offset: Offset(0, -10),
+              duration: Duration(milliseconds: 250),
+              delay: Duration(milliseconds: 100),
+              child: NavBarLogo(
+                height: MediaQuery.of(context).size.height * 0.035,
+              ),
+            ),
+      actions: [
+        for (int i = 0; i < _sectionsName.length; i++)
+          _appBarActions(_sectionsName[i], i, _sectionsIcons[i], _themeProv),
+        const SizedBox(width: 15.0),
+        SizedBox(
+          height: 30.0,
+          child: Switch(
+            inactiveTrackColor: Colors.grey,
+            value: !_themeProv.lightTheme,
+            onChanged: (value) {
+              _themeProv.lightTheme = !value;
+            },
+            activeColor: kPrimaryColor,
+          ),
+        ),
+        const SizedBox(width: 15.0),
+      ],
+    );
+  }
+
+  Widget _appBarMobile(ThemeProvider theme) {
+    return Drawer(
+      child: Material(
+        color: theme.lightTheme ? Colors.white : Colors.grey[900],
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 25.0, 0, 0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: NavBarLogo(
+                    height: 28,
+                  ),
+                ),
+                Divider(
+                  color: theme.lightTheme ? Colors.grey[200] : Colors.white,
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.light_mode,
+                    color: kPrimaryColor,
+                  ),
+                  title: Text("Dark Mode",
+                      style: TextStyle(
+                          color: theme.lightTheme ? Colors.black : Colors.white)),
+                  trailing: Switch(
+                    inactiveTrackColor: Colors.grey,
+                    value: !theme.lightTheme,
+                    onChanged: (value) {
+                      theme.lightTheme = !value;
+                    },
+                    activeColor: kPrimaryColor,
+                  ),
+                ),
+                Divider(
+                  color: theme.lightTheme ? Colors.grey[200] : Colors.white,
+                ),
+                for (int i = 0; i < _sectionsName.length; i++)
+                  _appBarActions(_sectionsName[i], i, _sectionsIcons[i], theme),
+                
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SectionsBody extends StatelessWidget {
+  final ScrollController scrollController;
+  final int sectionsLength;
+  final Widget Function(int) sectionWidget;
+
+  const SectionsBody({
+    Key key,
+    this.scrollController,
+    this.sectionsLength,
+    this.sectionWidget,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: ListView.builder(
+        // physics: !kIsWeb ? ScrollPhysics() : NeverScrollableScrollPhysics(),
+        controller: scrollController,
+        itemCount: sectionsLength,
+        itemBuilder: (context, index) => sectionWidget(index),
+      ),
     );
   }
 }
