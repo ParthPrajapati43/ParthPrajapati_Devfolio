6d8378aa20cd53dff4724c4a11897fcc0536474b
Parth Prajapati <parthy4399@gmail.com>
2021-10-16 12:11:48 +0530

    Numm Safety removed

--- lib/sections/skills/skills.dart
+++ lib/sections/skills/skills.dart
@@ -1,3 +1,5 @@
+// @dart=2.9
+
 import 'package:flutter/material.dart';
 import 'package:parth_prajapati_devfolio/sections/skills/desktop_skills.dart';
 import 'package:parth_prajapati_devfolio/sections/skills/mobile_skills.dart';
